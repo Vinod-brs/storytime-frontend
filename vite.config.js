d623108d4c54142483901b92fa6952c28c18474c
Vinod-brs <kingvinodkumart@gmail.com>
2025-01-27 10:04:55 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: parseInt(import.meta.env.VITE_PORT) || 3000, // Default to 3000 if VITE_PORT is not set
+    port: process.env.PORT || 3000, // Default to 3000 if VITE_PORT is not set
   },
 })
