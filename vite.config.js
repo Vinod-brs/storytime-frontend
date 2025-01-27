676e0c021f54fb7c993331bc74939b338c4035d9
Vinod-brs <kingvinodkumart@gmail.com>
2025-01-27 10:01:36 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    port: parseInt(import.meta.env.VITE_PORT) || 3000, // Default to 3000 if VITE_PORT is not set
   },
-  // base: '/storytime-frontend/'  
 })
